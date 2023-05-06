559036894d9764721af28b81e8ae53821dd9703e
FFigueroa17 <00019921@uca.edu.sv>
2023-05-05 18:21:13 -0600

    🚧 Add API base

---
+++ TEST/PoliFitnessApp/models/Post.model.js
@@ -0,0 +1,34 @@
+const Mongoose = require("mongoose");
+const Schema = Mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  image: {
+    //URL's
+    type: String,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  hidden: {
+    type: Boolean,
+    default: false
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+}, { timestamps: true });
+
+module.exports = Mongoose.model("Post", PostSchema);
